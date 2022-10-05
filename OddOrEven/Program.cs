7df93dfe47e360585f0dd520384246db5a487ad8
Sorokin0 <s.aleksander.99.09@gmail.com>
2022-10-05 21:02:45 +0300

    add week 3 code

---
+++ OddOrEven/Program.cs
@@ -0,0 +1,13 @@
+﻿
+Console.WriteLine("Enter number:");
+int userNum = Convert.ToInt32(Console.ReadLine());
+
+int result = userNum % 2;
+
+if (result != 0)
+{
+    Console.WriteLine("You number is odd.");
+} else
+{
+    Console.WriteLine("You number is even.");
+}
